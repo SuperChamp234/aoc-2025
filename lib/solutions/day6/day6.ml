115947b13f2b2a1042529f975c4992866164bd6f
Zain Siddavatam <dev@zainsv.me>
2025-12-10 12:27:29 +0530

    Added part 1 day 6

--- lib/solutions/day6/day6.ml
+++ lib/solutions/day6/day6.ml
@@ -1,3 +1,43 @@
+(* Part 1 functions *)
+let process_input raw =
+  let i_l = String.split_on_char '\n' (String.trim raw) in
+  let rec loop i_l num_ls op_l =
+    match i_l with
+    | [] -> (List.rev num_ls, List.rev op_l)
+    | "" :: rest -> loop rest num_ls op_l
+    | h :: rest ->
+        let arr =
+          List.filter
+            (fun x -> x <> "")
+            (String.split_on_char ' ' (String.trim h))
+        in
+        if Str.string_match (Str.regexp "[0-9]+") (List.hd arr) 0 then
+          loop rest (List.map int_of_string arr :: num_ls) op_l
+        else if Str.string_match (Str.regexp "[\\+,\\*]") (List.hd arr) 0 then
+          loop rest num_ls (arr :: op_l)
+        else failwith "Invalid input!"
+  in
+  loop i_l [] []
+
+let calc_int nums op =
+  match op with
+  | "*" -> List.fold_left ( * ) 1 nums
+  | "+" -> List.fold_left ( + ) 0 nums
+  | _ -> failwith "Invalid Op?"
+
+let rec get_heads_int nums_l acc new_nums_l =
+  match nums_l with
+  | [] -> (List.rev acc, List.rev new_nums_l)
+  | h :: t -> get_heads_int t (List.hd h :: acc) (List.tl h :: new_nums_l)
+
+let rec calculate nums_l ops_l acc =
+  match ops_l with
+  | [] -> acc
+  | op :: rest ->
+      let heads, new_nums_l = get_heads_int nums_l [] [] in
+      calculate new_nums_l rest (acc + calc_int heads op)
+
+(* Part 2 functions *)
 let get_num_string s =
   let arr = String.to_seq s |> List.of_seq in
   let buf = Buffer.create (List.length arr) in
@@ -80,6 +120,13 @@ let parselines lines =
 let solve filename =
   let raw = Utils.read_whole_file filename in
   let lines = String.split_on_char '\n' raw in
+  
+  (* Part 1 *)
+  let nums_l, op_l = process_input raw in
+  let part1 = calculate nums_l (List.hd op_l) 0 in
+  
+  (* Part 2 *)
   let (nums_seq_l, ops_s) = parselines lines in
   let part2 = calculator2 nums_seq_l ops_s in
-  Printf.printf "part2: %d\n" part2
+  
+  Printf.printf "part1: %d\npart2: %d\n" part1 part2
